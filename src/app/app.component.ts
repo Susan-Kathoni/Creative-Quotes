6dfa6f1b4be5701ced57e81069d829bf6066c6f5
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-09-28 14:28:29 +0300

    Modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Quotes } from './quotes';
+import { Quote } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Creative-Quotes';
-  quote: string[];
-
-  constructor() {
-    this.quote = [' ', ' ', ' ']
-  }
+  // quotes: Quote[] = [
+  //   { id: 1, quoteDescription: 'A good day starts early' },
+  //   { id: 2, quoteDescription: 'Hardwork pays' },
+  //   { id: 3, quoteDescription: 'You become what you eat' },
+  //   { id: 4, quoteDescription: 'You can learn Angular' },
+  // ];
 }
---
+++ src/app/quote-detail/quote-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-detail',
+  templateUrl: './quote-detail.component.html',
+  styleUrls: ['./quote-detail.component.css']
+})
+export class QuoteDetailComponent implements OnInit {
+
+  // @Input() quotes:  Quotes;
+  // constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
