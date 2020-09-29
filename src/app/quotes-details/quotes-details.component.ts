6f3c03948ebd4ee316d1c9dd6ad238e7b9c44456
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-09-29 17:39:33 +0300

    Replace previous quote-details component

---
+++ src/app/quotes-details/quotes-details.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit,Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '@angular/compiler';
+
+@Component({
+  selector: 'app-quotes-details',
+  templateUrl: './quotes-details.component.html',
+  styleUrls: ['./quotes-details.component.css']
+})
+    
+export class QuotesDetailsComponent implements OnInit {
+ 
+    @Input() quote: Quote;
+    @Output() isDisliked = new EventEmitter<boolean>();
+  
+  quoteDisliked(disliked:boolean){
+    this.isDisliked.emit(disliked);
+  }
+
+  constructor() { }
+
+  ngOnInit(): {
+  }
+
+}
