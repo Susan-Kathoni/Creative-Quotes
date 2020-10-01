1d933afb8c4e2fcd5b769e7df5dad429c9fb6625
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-10-01 17:02:44 +0300

    Make changes to some functions.

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -1,36 +1,21 @@
-import { Component, OnInit,Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quotes } from '../quotes';
+import { Quote } from '@angular/compiler';
 
 @Component({
   selector: 'app-quotes-details',
   templateUrl: './quotes-details.component.html',
-  styleUrls: ['./quotes-details.component.css']
+  styleUrls: ['./quotes-details.component.css'],
 })
-    
 export class QuotesDetailsComponent implements OnInit {
- 
-    @Input() quote: Quotes;
-    @Output() isDisliked = new EventEmitter<boolean>();
-  
-  quoteDisliked(isDisliked:boolean){
+  @Input() quote: Quotes;
+  @Output() isDisliked = new EventEmitter<boolean>();
+
+  quoteDelete(isDisliked: boolean) {
     this.isDisliked.emit(isDisliked);
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() : void {
-  }
-
+  ngOnInit(): void {}
 }
-
-
-
- 
-
-
-
-
-
-
- 
-
