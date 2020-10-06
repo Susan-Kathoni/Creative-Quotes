de915351986ee0671ab0fdcea3a089514a00a926
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-10-06 09:34:06 +0300

    Import navidation bar component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { QuotesDetailsComponent } from './quotes-details/quotes-details.componen
 import { DateCountPipe } from './date-count.pipe';
 import { QuoteInputFormComponent } from './quote-input-form/quote-input-form.component';
 import { FormsModule } from '@angular/forms';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 
 @NgModule({
-  declarations: [AppComponent, QuoteComponent, QuotesDetailsComponent, DateCountPipe, QuoteInputFormComponent],
+  declarations: [AppComponent, QuoteComponent, QuotesDetailsComponent, DateCountPipe, QuoteInputFormComponent, NavigationBarComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
